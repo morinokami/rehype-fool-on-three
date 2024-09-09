6c78729b2d23b3b3670c155a0615194e899aa5c3
Shinya Fujino <shf0811@gmail.com>
2024-09-10 00:39:57 +0900

    rehype-fool-on-three (#1)

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,1 +1,26 @@
-// TODO:
+import fs from "node:fs";
+import { rehype } from "rehype";
+import { describe, expect, test } from "vitest";
+
+import rehypeFooleOnThree from "./index";
+
+function readFixture(name: string): string {
+	return fs.readFileSync(
+		new URL(`./fixtures/${name}`, import.meta.url),
+		"utf8",
+	);
+}
+
+describe("rehypeFoolOnThree", async () => {
+	test.each([
+		["a.html", "a.out.html"],
+		["b.html", "b.out.html"],
+	])("%s", async (input, expected) => {
+		const file = await rehype()
+			.data("settings", { fragment: true })
+			.use(rehypeFooleOnThree)
+			.process(readFixture(input));
+
+		expect(file.toString()).toBe(readFixture(expected));
+	});
+});
